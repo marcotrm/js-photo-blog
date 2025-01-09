a48a38f8a64020a0e66af6c8ab5cc2ad838d8c97
marco torremacco <mtorremacco@gmail.comm>
2025-01-09 16:22:18 +0100

    feat add third milestone + responsive

--- script.js
+++ script.js
@@ -1,21 +1,25 @@
 // DOM ELMS
-const postItElm = document.querySelector('.card')
+const postItElm = document.querySelector('.row')
 //function
 function newCard() {
+    postItElm.innerHTML = '';
+
     axios.get('https://jsonplaceholder.typicode.com/photos?_limit=6')
         .then(function (response) {
             const infoPostItElm = response.data; 
             infoPostItElm.forEach(card => {
                 postItElm.innerHTML += `
+                    <div class="col">
                     <div class="card">
                         <div class="pin">
                             <img src="img/pin.svg" alt="pin">
                         </div>
-                        <img src="${card.url}" alt="">
+                        <img src=${card.url} alt="">
                         <h4>${card.title}</h4>
-                    </div>`
+                    </div>
+                </div>`
                 ;
             });
         });
-} 
+}
 newCard()
